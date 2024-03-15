6df3863f4a4cc1505a579f003cfd0aabfebaeaac
Humberto de Moraes Lanzieri Filho <134455113+hlanzieri@users.noreply.github.com>
2024-03-15 11:50:15 -0300

    Create relacaodiscipl.sql

---
+++ relacaodiscipl.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE Disciplinas (
+);
+
+CREATE TABLE Departamento (
+);
+
+CREATE TABLE Curso (
+);
+
+CREATE TABLE Professor (
+);
+
+CREATE TABLE Aluno (
+);
+
+CREATE TABLE Possui (
+);
